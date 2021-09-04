152815e686073ed3fdb064c0405e257ffadb41e4
SeungHyun-534 <22000534@handong.edu>
2021-09-04 23:04:09 +0900

    2

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,10 @@
+package com.example.lab1;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("first commit");
+		System.out.println("second commit");
+	}
+}
